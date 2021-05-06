6064e0c3a097079a1f95d6b35468d17788e56994
Warren Humphreys <warrenhumphreys@gmail.com>
2021-05-06 14:50:04 +0100

    Add files via upload

---
+++ Demo SQLITE/DemoLibrary/SqliteDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SqliteDataAccess
+    {
+        public static List<PersonModel> LoadPeople()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PersonModel>("select * from Person", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SavePerson(PersonModel person)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Person (FirstName, LastName, EmailAddress) values (@FirstName, @LastName, @EmailAddress)", person);
+
+                // exception error thrown, some syntax in the code or characters in the email. Most likely the list
+                // code is wrong and is not listing the email input field correctly. To look at tomorrow.
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
